4f168296a9dc2ec30480aedfb91eefe21bc5e903
David Borrego <dborrego@redhat.com>
2022-03-01 12:59:22 +0100

    print headers

--- src/main.go
+++ src/main.go
@@ -17,22 +17,37 @@ type vmContext struct {
 
 // Own context overriding types.DefaultVMContext
 func (*vmContext) NewPluginContext(contextID uint32) types.PluginContext {
-	return &wasmContext{}
+	return &pluginContext{}
 }
 
-type wasmContext struct {
-	// Embed the default plugin context here,
-	// so that we don't need to reimplement all the methods.
+type pluginContext struct {
 	types.DefaultPluginContext
+}
+
+// Override types.DefaultPluginContext.
+func (*pluginContext) NewHttpContext(contextID uint32) types.HttpContext {
+	return &httpHeaders{contextID: contextID}
+}
+
+type httpHeaders struct {
+	types.DefaultHttpContext
 	contextID uint32
 }
 
 // Override types.DefaultPluginContext.
-func (ctx *wasmContext) OnPluginStart(pluginConfigurationSize int) types.OnPluginStartStatus {
+func (ctx *pluginContext) OnPluginStart(pluginConfigurationSize int) types.OnPluginStartStatus {
 	proxywasm.LogInfo("OnPluginStart from Go!" + time.Now().GoString())
 
 	return types.OnPluginStartStatusOK
 }
 
-// Override types.DefaultPluginContext.
-func (*wasmContext) NewHttpContext(uint32) types.HttpContext { return &types.DefaultHttpContext{} }
+func (ctx *httpHeaders) OnHttpRequestHeaders(numHeaders int, endOfStream bool) types.Action {
+	h, err := proxywasm.GetHttpRequestHeaders()
+	if err != nil {
+		proxywasm.LogCriticalf("Failed to get request headers: %v", err)
+	}
+	for range h {
+		proxywasm.LogInfof("Request header <-- %s: %s", h[0], h[1])
+	}
+	return types.ActionContinue
+}
